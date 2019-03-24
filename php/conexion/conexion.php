87fcaae41d8c3db9d9754fc5f044e85a6f76b8c7
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2019-03-23 20:00:50 -0700

    agrego la conexion

---
+++ php/conexion/conexion.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'config.php';
+
+class Conexion {
+    protected $con;
+
+    public function __construct(){
+        $this->con= new mysqli(
+            BD_HOST,
+            BD_USER,
+            BD_PASS,
+            BD_NOMBRE
+        );
+
+        if($this->con -> connect_errno){
+            echo "Hubo un fallo de conexion: ".$this->con->connect_errno;
+            return;
+        }
+
+        $this->con->set_charset(BD_CHARSET);
+    }
+}
+
+?>
---
+++ php/conexion/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BD_HOST','localhost');
+define('BD_USER','root');
+define('BD_PASS','');
+define('BD_NOMBRE','bd_vinculacion3');
+define('BD_CHARSET','utf8');
+
+?>
