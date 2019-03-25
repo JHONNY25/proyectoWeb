37ae7045058cb3acfa1ed40b7fd5dbf05483f477
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2019-03-25 00:32:48 -0700

    agrego clase personal

---
+++ php/procesamiento/personal.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once '../model/conexion.php';
+
+class Personal extends Conexion{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function listarPersonas(){
+        $sql = "CALL ver_persona_administrador()";
+        $ejecuta = $this->con->query($sql);
+
+        $respuesta = $ejecuta->fetch_all(MYSQLI_ASSOC);
+
+        if($respuesta){
+            return $respuesta;
+
+            $respuesta->close();
+            $this->con->close();
+        }
+    }
+
+    public function registrarPersonas(){
+        
+    }
+
+    public function modificarPersonas(){
+        
+    }
+}
