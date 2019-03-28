bc2fc555ce73d038680c04414389ae2efbadc56d
Tebano <tebano@outlook.com>
2019-03-28 00:26:32 -0700

    Registro usuario actualizdo :v

--- php/formularios/NuevoUsuario.php
+++ php/formularios/NuevoUsuario.php
@@ -1,12 +1,27 @@
+<script src="/proyectoWeb/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+<script src="/proyectoWeb/js/sweetalert2.all.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="/proyectoWeb/css/sweetalert2.min.css">
+<body>
+  <script type="text/javascript">
+  function alertaExito(){
+    Swal.fire(
+    'Éxito',
+    'El usuario se registró correctamente',
+    'success'
+    )
+  }
+  </script>
+</body>
 <?php
 
 //CONEXION
 require '../procesamiento/coneccion.php';
 require '../Validaciones/Valida_NuevoUsuario.php';
 
+$mysql = new coneccion();
+$conexion = $mysql->get_connection();
 if(!empty($_POST)){
-  $mysql = new coneccion();
-  $conexion = $mysql->get_connection();
+
 
   //----------------------------------------------------------------------------VARIABLES DE FORMULARIO
   $nombre = $_POST['nombre'];
@@ -19,6 +34,9 @@ if(!empty($_POST)){
   $nombreUsuario = $_POST['nombreUsuario'];
   $correo = $_POST['correo'];
   $telefono = $_POST['telefono'];
+  $idCarrera = $_POST['carrera'];
+
+
 
   //----------------------------------------------------------------------------VARIABLES GENERALES
   $tipo = 1;
@@ -56,6 +74,18 @@ if(!empty($_POST)){
   else if($tipoCB == 'Administrativo'){
     $tipo = '2';
   }
+
+  if ($tipoCB=='Alumno') {
+    $contrasena = $numeroControl;
+    $confirmaContra = $numeroControl;
+  }
+
+  if ($tipoCB=='Administrativo') {
+    $numeroControl = null;
+    $idCarrera=null;
+    $creditos = null;
+  }
+
 
 //------------------------------------------------------------------------------VALIDA USUARIO REPETIDO
   if(existeUsuario($nombreUsuario)){
@@ -168,6 +198,7 @@ if(!empty($_POST)){
 
 
 
+
     //LLAMAR AL PROCEDIMIENTO PARA INSERTAR USUARIO "SSI" SIGNIFICA "STRING STRING INT", ES DECIR, EL TIPO DE VALORES A INGRESAR
     $stm = $conexion->prepare("CALL registrar_usuario(?,?,?)");
     $stm->bind_param("ssi",$nombreUsuario,$contrasena,$tipo);
@@ -189,14 +220,21 @@ if(!empty($_POST)){
 
 
     //AGREGAMOS LOS DATOS DE LA PERSONA AL USUARIO
-      $stm = $conexion->prepare("CALL registrar_persona(?,?,?,?,?,?,?,?)");
-      $stm->bind_param("ssssssdi",$nombre,$aPat,$aMat,$numeroControl,$correo,$telefono,$creditos,$idBD);
+      $stm = $conexion->prepare("CALL registrar_persona(?,?,?,?,?,?,?,?,?)");
+      $stm->bind_param("ssssssdii",$nombre,$aPat,$aMat,$numeroControl,$correo,$telefono,$creditos,$idBD,$idCarrera);
       $stm->execute();
       $stm->close();
 
 
-    $conexion->close();
+
     $registroExitoso=1;
+    ?>
+    <script type="text/javascript">
+      alertaExito();
+    </script>
+    <?php
+
+    //header('Location: ../tablas/personal.php');
     //--------------------------------------------------------------------------CERRAR IF ERRORES
   }
 
@@ -244,12 +282,13 @@ if(!empty($_POST)){
 
 
 
-<div class="container">
 
-  <!-- Outer Row -->
-  <div class="row justify-content-center">
+  <div class="container">
 
-    <div class="col-xl-10 col-lg-12 col-md-9">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
 
       <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
@@ -264,12 +303,43 @@ if(!empty($_POST)){
 
 
 
-                <form action="<?php $_SERVER['PHP_SELF'] ?>" class="user" method="post">
+                <form  action="<?php $_SERVER['PHP_SELF'] ?>" class="user" method="post">
                   <hr>
                   <h5>Datos Generales</h5>
+
+
+                  <div class="form-group" >
+                      <p>Tipo de usuario
+                      <select id="slctTipo" name="tipo"class="custom-select "  >
+                        <option value="Alumno">Alumno</option>
+                        <option value="Administrativo">Administrativo</option>
+                      </select>
+                      </p>
+                  </div>
+
+                  <div id="slctCarrera" class="form-group" >
+                      <p>Carrera
+                      <select name="carrera"class="custom-select "  >
+                        <?php
+                        $sql = "SELECT id_carrera,nombre FROM carrera";
+                        $result = $conexion->query($sql);
+                        if ($result->num_rows > 0) {
+
+                             while($row = $result->fetch_assoc()) {
+                               ?>
+                               <option value=<?php print($row['id_carrera']); ?>> <?php print($row['nombre']); ?> </option>
+                               <?php
+                             }
+                           }
+
+                         ?>
+
+                      </select>
+                      </p>
+                  </div>
+
+
                   <div class="form-group">
-
-
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR NOMBRE
                       if (!empty($_POST)) {
@@ -282,7 +352,9 @@ if(!empty($_POST)){
                      ?>
 
 
-                    <input type="text" maxlength="24" class="form-control form-control-user" name="nombre" aria-describedby="emailHelp" placeholder="Nombre">
+                    <input type="text" maxlength="24" class="form-control form-control-user" name="nombre" aria-describedby="emailHelp" placeholder="Nombre" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($nombre); ?> <?php
+                    } ?>>
                   </div>
 
 
@@ -290,14 +362,16 @@ if(!empty($_POST)){
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR APELLIDO PATERNO
                       if (!empty($_POST)) {
-                        if ($errorNombre==1) {
+                        if ($errorApPaterno==1) {
                           ?>
                           <p class="text-danger">X Campo requerido</p>
                           <?php
                         }
                       }
                      ?>
-                    <input type="text" maxlength="24" class="form-control form-control-user" name="aPat" aria-describedby="emailHelp" placeholder="Apellido Paterno">
+                    <input type="text" maxlength="24" class="form-control form-control-user" name="aPat" aria-describedby="emailHelp" placeholder="Apellido Paterno" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($aPat); ?> <?php
+                    } ?>>
                   </div>
 
 
@@ -305,14 +379,16 @@ if(!empty($_POST)){
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR APELLIDO MATERNO
                       if (!empty($_POST)) {
-                        if ($errorNombre==1) {
+                        if ($errorApMaterno==1) {
                           ?>
                           <p class="text-danger">X Campo requerido</p>
                           <?php
                         }
                       }
                      ?>
-                    <input type="text" maxlength="24" class="form-control form-control-user" name="aMat" aria-describedby="emailHelp" placeholder="Apellido Materno">
+                    <input type="text" maxlength="24" class="form-control form-control-user" name="aMat" aria-describedby="emailHelp" placeholder="Apellido Materno" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($aMat); ?> <?php
+                    } ?>>
                   </div>
 
 
@@ -332,7 +408,9 @@ if(!empty($_POST)){
                         }
                       }
                      ?>
-                    <input type="email" maxlength="50" class="form-control form-control-user" name="correo" aria-describedby="emailHelp" placeholder="Correo">
+                    <input type="email" maxlength="50" class="form-control form-control-user" name="correo" aria-describedby="emailHelp" placeholder="Correo" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($correo); ?> <?php
+                    } ?>>
                   </div>
 
 
@@ -348,7 +426,9 @@ if(!empty($_POST)){
                         }
 
                      ?>
-                    <input type="text" maxlength="10"class="form-control form-control-user" name="telefono" aria-describedby="emailHelp" placeholder="Teléfono">
+                    <input type="text" maxlength="10"class="form-control form-control-user" name="telefono" aria-describedby="emailHelp" placeholder="Teléfono" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($telefono); ?> <?php
+                    } ?>>
                   </div>
 
 
@@ -372,39 +452,47 @@ if(!empty($_POST)){
                         }
 
                      ?>
-                    <input type="text" maxlength="50" class="form-control form-control-user" name="nombreUsuario" aria-describedby="emailHelp" placeholder="Nombre de usuario">
+                      <input type="text" maxlength="50" class="form-control form-control-user" name="nombreUsuario" aria-describedby="emailHelp" placeholder="Nombre de usuario" <?php if (!empty($_POST)) {
+                        ?>value= <?php print($nombreUsuario);} ?>>
                   </div>
 
 
-                  <div class="form-group">
+                  <div id="nControl" class="form-group">
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR NUMERO DE CONTROL
                         if (!empty($_POST)) {
                           if ($errorControl==1) {
                             ?>
-                            <p class="text-danger">X Formato de numero de control invalido, por favor verifique el campo</p>
+                            <p class="text-danger">X Formato de número de control inválido, por favor verifique el campo</p>
                             <?php
                           }
                           if ($errorControlVacio==1) {
                             ?>
-                            <p class="text-danger">X Numero de control no puede estar vacío para alumno</p>
+                            <p class="text-danger">X Número de control no puede estar vacío</p>
 
                             <?php
                           }
                         }
 
                      ?>
-                    <input type="text" maxlength="8" class="form-control form-control-user" name="numeroControl" aria-describedby="emailHelp" placeholder="Número de control o clave">
+                    <input type="text" maxlength="8" class="form-control form-control-user" name="numeroControl" aria-describedby="emailHelp" placeholder="Número de control" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($numeroControl);} ?>>
+
+                    <div id="avisoContrasena" class="">
+                      <p class="text-danger">IMPORTANTE:</p>
+                      <p class="text-info">La contraseña del usuario será el número de control, el alumno deberá cambiarla al iniciar sesión</p>
+                    </div>
+                    <script type="text/javascript"> $("#avisoContrasena").hide(); </script>
                   </div>
 
 
-                  <div class="form-group">
+                  <div id="Contrasena" class="form-group">
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR NUMERO DE CONTROL
                         if (!empty($_POST)) {
                           if ($errorContra==1) {
                             ?>
-                            <p class="text-danger">X Este campo no puede estar vacio, debe ingresar una contraseña de mínimo 8 caracteres</p>
+                            <p class="text-danger">X Este campo no puede estar vacío, debe ingresar una contraseña de mínimo 8 caracteres</p>
                             <?php
                           }
                           if ($errorCoincidencia==1) {
@@ -415,38 +503,44 @@ if(!empty($_POST)){
                         }
 
                      ?>
-                    <input type="password" minlength=8 maxlength="50" class="form-control form-control-user" name="contrasena" placeholder="Contraseña">
+                    <input type="password" minlength=8 maxlength="50" class="form-control form-control-user" name="contrasena" placeholder="Contraseña" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($nombreUsuario);} ?>>
                   </div>
 
 
-                  <div class="form-group">
+                  <div id="confirmaContra" class="form-group">
                     <?php
                     //----------------------------------------------------------MUESTRA ERROR CONFIRMA CONTRASEÑA
                         if (!empty($_POST)) {
                           if ($errorContra2==1) {
                             ?>
-                            <p class="text-danger">X Este campo no puede estar vacio, debe ingresar una contraseña de mínimo 8 caracteres</p>
+                            <p class="text-danger">X Este campo no puede estar vacío, debe ingresar una contraseña de mínimo 8 caracteres</p>
                             <?php
                           }
                         }
 
                      ?>
-                    <input type="password"  minlength=8 maxlength="50" class="form-control form-control-user" name="confirmContra" placeholder="Confirma contraseña">
+                    <input type="password"  minlength=8 maxlength="50" class="form-control form-control-user" name="confirmContra" placeholder="Confirma contraseña" <?php if (!empty($_POST)) {
+                      ?>value= <?php print($nombreUsuario);} ?>>
                   </div>
 
-                  <div class="form-group" >
-                      <select name="tipo"class="custom-select "  >
-                        <option value="Alumno">Alumno</option>
-                        <option value="Administrativo">Administrativo</option>
-                      </select>
-                  </div>
 
                   <div class="form-group">
                   </div>
-                  <p><input type="submit" value="Registrar Usuario" href="#" class="btn btn-inicio btn-user btn-block" />
+                  <p><input id="boton" type="submit" value="Registrar Usuario" href="#" class="btn btn-inicio btn-user btn-block" />
                   </p>
 
                 </form>
+
+                <script type="text/javascript">
+
+                $("#slctCarrera").show();
+                $("#nControl").show();
+                $("#confirmaContra").hide();
+                $("#Contrasena").hide();
+                $("#avisoContrasena").show();
+
+                </script>
 
                 <?php
 
@@ -460,8 +554,43 @@ if(!empty($_POST)){
 
                  ?>
 
+<script type="text/javascript">
+$(document).ready(function(){
+                $("#slctTipo").change(function(){
+
+                  if ($(this).val()=="Administrativo") {
+                    $("#slctCarrera").hide();
+                    $("#nControl").hide();
+                    $("#confirmaContra").show();
+                    $("#Contrasena").show();
+                    $("#avisoContrasena").hide();
 
 
+
+
+                  }else{
+                    $("#slctCarrera").show();
+                    $("#nControl").show();
+                    $("#confirmaContra").hide();
+                    $("#Contrasena").hide();
+                    $("#avisoContrasena").show();
+                  }
+                });
+            });
+</script>
+
+
+
+
+<script type="text/javascript">
+$(function() {
+    $("#boton").click( function()
+         {
+
+         }
+    );
+});
+</script>
 
 
 
@@ -476,6 +605,7 @@ if(!empty($_POST)){
   </div>
 
 </div>
+<?php $conexion->close(); ?>
 
 
 
