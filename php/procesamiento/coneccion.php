f4730aba9b63f4ce47c86233535f23c28b3d9088
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2019-05-15 13:33:47 -0600

    .

--- php/procesamiento/coneccion.php
+++ php/procesamiento/coneccion.php
@@ -3,7 +3,7 @@
 class coneccion{
   public $conn;
   public function __construct(){
-    $this->conn = new mysqli("localhost","root","","vinculacion");
+    $this->conn = new mysqli("localhost","root","123456","vinculacion");
   }
   public function get_connection(){
     return $this->conn;
